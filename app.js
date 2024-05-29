de050e24af0283dd147654b3e0c06e1c1250ca65
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-05-29 10:36:55 -0400

    setting H1 element

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-import "./styles.css";
+const mainEl = document.querySelector("main");
+
+console.log(mainEl);
+
+//setting the background color using var method to grab item from the root
+mainEl.style.backgroundColor = "var(--main-bg)";
+//creating an HTML Element
+const h1 = document.createElement("h1")
